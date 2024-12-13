2fea3cae4632b80271ed8698efa9523a410b9bb0
Muhammed Sinan P <iam.muhammedsinan.p@gmail.com>
2024-12-13 12:46:29 +0530

    align google sign btn

--- src/components/forms/AuthForm.jsx
+++ src/components/forms/AuthForm.jsx
@@ -92,10 +92,10 @@ const AuthForm = ({ isRegister,formData,setFormData }) => {
               </NavLink>
             </div>
           )}
-          <div className="w-full border-b my-3"></div>
+          <div className="w-full  border-b my-3"></div>
           <button
             onClick={googleSignIn}
-            className="btn btn-sm btn-outline  border-brandColor hover:bg-brandColor hover:border-brandColor text-brandColor"
+            className="btn btn-sm btn-outline flex items-center justify-center gap-2  border-brandColor hover:bg-brandColor hover:border-brandColor text-brandColor"
           >
             <img src={googleIcon} alt="google icon" className="h-4" />
             <span className="text-sm">Continue with Google</span>
